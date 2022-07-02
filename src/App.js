7a014e0b6bb34673a83181e21ca6600a6e01bbb8
Jelle Baele <jellebaele@hotmail.com>
2022-07-02 11:30:19 +0200

    Setup basic routing + install mui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Orders from './pages/orders/Orders';
+import Products from './pages/products/Products';
+
 function App() {
-   return <div className>App</div>;
+   return (
+      <div>
+         <BrowserRouter>
+            <Routes>
+               <Route path="/login" element={<Login />} />
+               <Route path="/" element={<Home />} />
+               <Route path="/orders" element={<Orders />} />
+               <Route path="/products" element={<Products />} />
+            </Routes>
+         </BrowserRouter>
+      </div>
+   );
 }
 
 export default App;
