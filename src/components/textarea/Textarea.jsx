c7f7133de7f36e492782a1888a9f164db42daf32
Jelle Baele <jellebaele@hotmail.com>
2022-07-29 23:41:40 +0200

    Autoresize on mount

--- src/components/textarea/Textarea.jsx
+++ src/components/textarea/Textarea.jsx
@@ -3,33 +3,41 @@ import React, { useEffect, useRef, useState } from 'react';
 const Textarea = ({ name, onChange, register, disabled, className }) => {
    const textareaRef = useRef(null);
    const [value, setValue] = useState('');
+   const { ref, ...rest } = register(name);
 
    const textAreaChange = (event) => {
       setValue(event.target.value);
       onChange(event.target.value);
    };
 
-   const setTextareaHeight = () => {
+   // const setTextareaHeight = () => {
+   //    if (ref && ref.current) {
+   //       ref.current.style.height = '0px';
+   //       const scrollHeight = ref.current.scrollHeight;
+   //       ref.current.style.height = scrollHeight + 'px';
+   //    }
+   // };
+
+   useEffect(() => {
       if (textareaRef && textareaRef.current) {
          textareaRef.current.style.height = '0px';
          const scrollHeight = textareaRef.current.scrollHeight;
          textareaRef.current.style.height = scrollHeight + 'px';
       }
-   };
-
-   useEffect(() => {
-      setTextareaHeight();
    }, [value]);
 
    return (
       <textarea
-         ref={textareaRef}
+         ref={(e) => {
+            ref(e);
+            textareaRef.current = e; // you can still assign to ref
+         }}
          onChange={textAreaChange}
-         {...register(name)}
+         {...rest}
          disabled={disabled}
          className={`${className}`}
       >
-         {value}
+         {/* {value} */}
       </textarea>
    );
 };
