63298fe44e297d6a391d7f295176adcfdf9db0ae
Jelle Baele <jellebaele@hotmail.com>
2023-01-02 20:30:38 +0100

    Typo

--- client/src/features/User/form/LoginForm.jsx
+++ client/src/features/User/form/LoginForm.jsx
@@ -7,38 +7,38 @@ import './login-form.scss';
 import { ArrowForward } from '@mui/icons-material';
 
 const LoginForm = ({ onSubmit }) => {
-   const {
-      register,
-      handleSubmit,
-      formState: { errors },
-   } = useForm(LoginFormOptions);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm(LoginFormOptions);
 
-   return (
-      <form className="loginFormContainer" onSubmit={handleSubmit(onSubmit)}>
-         <FormInputGroup
-            label="Gebruikersnaam of e-mail"
-            name="usernameOrPassword"
-            type="text"
-            register={register}
-            error={errors.usernameOrPassword}
-            className="formInput"
-         />
+  return (
+    <form className="loginFormContainer" onSubmit={handleSubmit(onSubmit)}>
+      <FormInputGroup
+        label="Gebruikersnaam of e-mail"
+        name="usernameOrEmail"
+        type="text"
+        register={register}
+        error={errors.usernameOrEmail}
+        className="formInput"
+      />
 
-         <FormInputGroup
-            label="Wachtwoord"
-            name="password"
-            type="password"
-            register={register}
-            error={errors.password}
-            className="formInput"
-         />
-         <div className="buttonContainer">
-            <Button type="submit" className="button" endIcon={ArrowForward}>
-               Login
-            </Button>
-         </div>
-      </form>
-   );
+      <FormInputGroup
+        label="Wachtwoord"
+        name="password"
+        type="password"
+        register={register}
+        error={errors.password}
+        className="formInput"
+      />
+      <div className="buttonContainer">
+        <Button type="submit" className="button" endIcon={ArrowForward}>
+          Login
+        </Button>
+      </div>
+    </form>
+  );
 };
 
 export default LoginForm;
--- client/src/features/User/form/validation.js
+++ client/src/features/User/form/validation.js
@@ -2,84 +2,82 @@ import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 const editUserValidationSchema = Yup.object().shape({
-   firstName: Yup.string().required('Voornaam is verplicht.'),
-   lastName: Yup.string().required('Achternaam is verplicht.'),
-   email: Yup.string().email().required('E-mail is verplicht.'),
-   username: Yup.string().required('Gebruikersnaam is verplicht.'),
-   currentPassword: Yup.string()
-      .required('Wachtwoord is verplicht.')
-      .test(
-         'Correct password',
-         'Huidig wachtwoord niet correct',
-         function (value) {
-            return true;
-         }
-      ),
+  firstName: Yup.string().required('Voornaam is verplicht.'),
+  lastName: Yup.string().required('Achternaam is verplicht.'),
+  email: Yup.string().email().required('E-mail is verplicht.'),
+  username: Yup.string().required('Gebruikersnaam is verplicht.'),
+  currentPassword: Yup.string()
+    .required('Wachtwoord is verplicht.')
+    .test(
+      'Correct password',
+      'Huidig wachtwoord niet correct',
+      function (value) {
+        return true;
+      }
+    ),
 });
 
 export const UserEditFormOptions = {
-   resolver: yupResolver(editUserValidationSchema),
+  resolver: yupResolver(editUserValidationSchema),
 };
 
 const editPasswordValidationScehma = Yup.object().shape({
-   currentPassword: Yup.string()
-      .required('Wachtwoord is verplicht.')
-      .test(
-         'Correct password',
-         'Huidig wachtwoord niet correct',
-         function (value) {
-            return true;
-         }
-      ),
-   newPassword: Yup.string()
-      .required('Wachtwoord is verplicht')
-      .min(10, 'Het wachtwoord moet minimaal 10 karakters lang zijn'),
-   confirmNewPassword: Yup.string().oneOf(
-      [Yup.ref('newPassword'), null],
-      'Wachtwoorden moeten hetzelfde zijn.'
-   ),
+  currentPassword: Yup.string()
+    .required('Wachtwoord is verplicht.')
+    .test(
+      'Correct password',
+      'Huidig wachtwoord niet correct',
+      function (value) {
+        return true;
+      }
+    ),
+  newPassword: Yup.string()
+    .required('Wachtwoord is verplicht')
+    .min(10, 'Het wachtwoord moet minimaal 10 karakters lang zijn'),
+  confirmNewPassword: Yup.string().oneOf(
+    [Yup.ref('newPassword'), null],
+    'Wachtwoorden moeten hetzelfde zijn.'
+  ),
 });
 
 export const PasswordEditFormOptions = {
-   resolver: yupResolver(editPasswordValidationScehma),
+  resolver: yupResolver(editPasswordValidationScehma),
 };
 
 const loginValidationSchema = Yup.object().shape({
-   usernameOrPassword: Yup.string().required(
-      'Gebruikersnaam/e-mail is verplicht'
-   ),
-   password: Yup.string().required('Wachtwoord is verplicht'),
+  usernameOrEmail: Yup.string().required('Gebruikersnaam/e-mail is verplicht'),
+  password: Yup.string().required('Wachtwoord is verplicht'),
 });
 
 export const LoginFormOptions = {
-   resolver: yupResolver(loginValidationSchema),
+  resolver: yupResolver(loginValidationSchema),
 };
 
 const registerValidationSchema = Yup.object().shape({
-   surname: Yup.string()
-      .required('Voornaam is verplicht')
-      .min(2, 'Voornaam moet minstens 2 karakters lang zijn')
-      .max(30, 'Voornaam mag niet langer zijn dan 30 karakters'),
-   lastname: Yup.string()
-      .required('Achternaam is verplicht')
-      .min(2, 'Achternaam moet minstens 2 karakters lang zijn')
-      .max(30, 'Achternaam mag niet langer zijn dan 30 karakters'),
-   username: Yup.string()
-      .required('Gebruikersnaam is verplicht')
-      .min(2, 'Gebruikers moet minstens 2 karakters lang zijn')
-      .max(30, 'Gebruikersnaam mag niet langer zijn dan 30 karakters')
-      .test(
-         'Correct username',
-         'Gebruikersnaam is reeds in gebruik',
-         function (value) {
-            return true;
-         }
-      ),
-   email: Yup.string()
-      .email('Geen geldig e-mail adres')
-      .required('E-mail adres is verplicht'),
+  surname: Yup.string()
+    .required('Voornaam is verplicht')
+    .min(2, 'Voornaam moet minstens 2 karakters lang zijn')
+    .max(30, 'Voornaam mag niet langer zijn dan 30 karakters'),
+  lastname: Yup.string()
+    .required('Achternaam is verplicht')
+    .min(2, 'Achternaam moet minstens 2 karakters lang zijn')
+    .max(30, 'Achternaam mag niet langer zijn dan 30 karakters'),
+  username: Yup.string()
+    .required('Gebruikersnaam is verplicht')
+    .min(2, 'Gebruikers moet minstens 2 karakters lang zijn')
+    .max(30, 'Gebruikersnaam mag niet langer zijn dan 30 karakters')
+    .test(
+      'Correct username',
+      'Gebruikersnaam is reeds in gebruik',
+      function (value) {
+        return true;
+      }
+    ),
+  email: Yup.string()
+    .email('Geen geldig e-mail adres')
+    .required('E-mail adres is verplicht'),
 });
 
 export const RegisterFormOptions = {
-   resolver: yupResolver(registerValidationSchema),
+  resolver: yupResolver(registerValidationSchema),
 };
