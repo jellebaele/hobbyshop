6e452ba6e229351c20a919dfe38fb3acffcebb74
Jelle Baele <jellebaele@hotmail.com>
2023-09-12 18:46:06 +0200

    Add profilepage

--- client_v2/src/App.tsx
+++ client_v2/src/App.tsx
@@ -1,11 +1,12 @@
 import './app.scss'
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import ProductsListPage from './pages/ProductsListPage';
+import ProductsListPage from './pages/product/ProductsListPage';
 import NotExistsPage from './pages/NotExistsPage';
 import Navbar from './components/ui/navbar/Navbar';
-import SingleProductPage from './pages/SingleProductPage';
+import SingleProductPage from './pages/product/SingleProductPage';
 import LoginPage from './pages/LoginPage';
+import ProfilePage from './pages/user/ProfilePage';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path="products" element={<ProductsListPage />} />
           <Route path="products/:productId" element={<SingleProductPage />} />
+          <Route path='/profile' element={<ProfilePage />} />
           <Route path="*" element={<NotExistsPage />} />
         </Route>
         <Route path='/login' element={<LoginPage />} />
