7042d59ca3da963036c31cfdbd76b6cf0a13b79d
Jelle Baele <jellebaele@hotmail.com>
2023-09-17 17:38:20 +0200

    Add arrow icon to button

--- client_v2/src/features/products/higlightedProducts/HighlightedProducts.tsx
+++ client_v2/src/features/products/higlightedProducts/HighlightedProducts.tsx
@@ -2,6 +2,7 @@ import './highlightedProducts.scss'
 import { Link } from 'react-router-dom'
 import Button from '../../../components/ui/button/Button'
 import ProductList from '../productList/ProductList'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const HighlightedProducts = () => {
   return (
@@ -9,9 +10,12 @@ const HighlightedProducts = () => {
       <div className="top">
         <h2 className="containerTitle">Uitgelichte producten</h2>
         <Link to={'/products'}>
-          <Button>Alle producten</Button>
+          <Button className='button'>
+            Alle producten <ArrowForwardIcon className='icon' />
+          </Button>
         </Link>
       </div>
+
       <div className="content">
         <ProductList />
       </div>
