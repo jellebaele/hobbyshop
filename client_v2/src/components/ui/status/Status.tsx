f69a2cd35164903291083e07e90176530d1236bf
Jelle Baele <jellebaele@hotmail.com>
2023-09-18 21:43:16 +0200

    Create status component

---
+++ client_v2/src/components/ui/status/Status.tsx
@@ -0,0 +1,13 @@
+import './status.scss'
+
+type StatusProps = {
+  status: 'Actief' | 'Inactief',
+  className?: string
+}
+const Status = ({ status, className }: StatusProps) => {
+  return (
+    <div className={`status ${status.toLowerCase()} ${className}`}>{status}</div>
+  )
+}
+
+export default Status
