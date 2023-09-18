6d8966c85c5d4c9ae073f06754029db8e294dfb8
Jelle Baele <jellebaele@hotmail.com>
2023-09-18 21:45:02 +0200

    Implement single product as a modal

--- client_v2/src/components/ui/modal/Modal.tsx
+++ client_v2/src/components/ui/modal/Modal.tsx
@@ -1,26 +1,14 @@
 import './modal.scss'
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router'
 
 type ModalProps = {
+  visible: boolean,
+  setInvisible: () => void,
   children?: React.ReactNode,
 }
 
-const Modal = ({ children }: ModalProps) => {
-  const [visible, setVisible] = useState(false)
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    setVisible(true)
-  }, [])
-
-  const handleClose = () => {
-    setVisible(false);
-    setTimeout(() => navigate(-1), 300)
-  }
-
+const Modal = ({ visible, setInvisible, children }: ModalProps) => {
   return (
-    <div className={`modalWrapper ${visible ? 'active' : ''}`} onClick={handleClose}>
+    <div className={`modalWrapper ${visible ? 'active' : ''}`} onClick={setInvisible}>
       <div className={`modalContainer ${visible ? 'active' : ''}`} onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => e.stopPropagation()}>
         {children}
       </div>
--- client_v2/src/features/products/singleProduct/SingleProduct.tsx
+++ client_v2/src/features/products/singleProduct/SingleProduct.tsx
@@ -1,8 +1,55 @@
+import './singleProduct.scss'
+import { useParams } from "react-router-dom"
 import Modal from "../../../components/ui/modal/Modal"
+import { useAppSelector } from "../../../context/hooks";
+import { selectPostById } from "../productsSlice";
+import EditIconNote from '@mui/icons-material/EditNote'
+import CloseIcon from '@mui/icons-material/Close';
+import Status from '../../../components/ui/status/Status';
+import { useSmoothVisibility } from '../../../hooks/useSmoothClose';
 
 const SingleProduct = () => {
+  const { productId } = useParams();
+  const { visible, setInvisible } = useSmoothVisibility();
+
+  const product = useAppSelector((state) => selectPostById(state, productId))
+
+  const renderProduct = () => {
+    if (!product) {
+      return <h2>Post not found!</h2>
+    } else {
+      return (
+        <>
+          <div className="top">
+            <h2>{product.name}</h2>
+            <div className="iconContainer">
+              <EditIconNote className='icon editIcon' />
+              <CloseIcon className='icon editIcon' onClick={() => setInvisible()} />
+            </div>
+          </div>
+          <div className="center">
+            <div className='productProperty'>Aantal: {product.amount} {product.unit}</div>
+            <div className='productProperty'>Eigenaar: {product.user}</div>
+            <div className='productProperty'>Categorie: {product.category}</div>
+            <div className='productProperty'>Laatst bijgewerkt: {product.dateUpdated.toDateString()}</div>
+            <div className='productProperty'>Status: <Status status={product.status} /></div>
+          </div>
+          <div className="bottom">
+
+          </div>
+        </>
+      )
+    }
+  }
+
   return (
-    <div><Modal>Single product</Modal></div>
+    <>
+      <Modal visible={visible} setInvisible={setInvisible}>
+        <div className="singleProductContainer">
+          {renderProduct()}
+        </div>
+      </Modal>
+    </>
   )
 }
 
