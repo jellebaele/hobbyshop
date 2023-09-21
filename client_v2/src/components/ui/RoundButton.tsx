eb9bed852cf9b30dfa33f41150d6c8aaa220d56d
Jelle Baele <jellebaele@hotmail.com>
2023-09-21 07:40:16 +0200

    Create a round button

---
+++ client_v2/src/components/ui/RoundButton.tsx
@@ -0,0 +1,22 @@
+import '../../assets/styles/components/ui/roundButtons.scss'
+import React from 'react'
+import Button from './Button'
+
+type RoundButtonProps = {
+  onClick?: (event: React.MouseEvent) => void
+  className?: string,
+  children?: React.ReactNode
+}
+
+const RoundButton = ({ onClick, className, children }: RoundButtonProps) => {
+  return (
+
+    <Button className={`roundButton ${className}`} onClick={onClick}>
+      {children}
+    </Button>
+
+
+  )
+}
+
+export default RoundButton
