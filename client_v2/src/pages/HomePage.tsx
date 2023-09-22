1d5c7f0950779b6192e39ff5c8f583c94b3f0776
Jelle Baele <jellebaele@hotmail.com>
2023-09-22 07:29:44 +0200

    Delete HighlightedProducts component and move into homepage

--- client_v2/src/pages/HomePage.tsx
+++ client_v2/src/pages/HomePage.tsx
@@ -1,11 +1,30 @@
+import { Link } from "react-router-dom"
 import "../assets/styles/pages/homePage.scss"
-import HighlightedProducts from "../features/products/HighlightedProducts"
+import Button from "../components/ui/Button"
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ProductList from "../features/products/ProductList";
 
 const HomePage = () => {
+  const renderHighlightedProducts = () => {
+    return (<div className="higlightedProductsContainer">
+      <div className="top">
+        <h2>Uitgelichte producten</h2>
+        <Link to={'/products'}>
+          <Button className='button'>
+            Alle producten <ArrowForwardIcon className='icon' />
+          </Button>
+        </Link>
+      </div>
+      <div>
+        <ProductList />
+      </div>
+    </div>)
+  }
+
   return (
     <div className='homeContainer'>
-      <HighlightedProducts />
-      <HighlightedProducts />
+      {renderHighlightedProducts()}
+      {renderHighlightedProducts()}
     </div >
   )
 }
