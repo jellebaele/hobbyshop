1b27c22c70a6eaa12b9f938f19b2aad3c346d30b
Jelle Baele <jellebaele@hotmail.com>
2023-10-04 19:15:00 +0200

    Adjust iconbutton

--- client_v2/src/components/ui/IconButton.tsx
+++ client_v2/src/components/ui/IconButton.tsx
@@ -1,27 +1,32 @@
-import React from 'react'
+import '../../assets/styles/components/ui/iconButton.scss';
+import React, { ReactElement } from 'react';
+import Button from './Button';
 
 type IconButtonProps = {
-  iconLeft?: React.ReactNode,
-  iconRight?: React.ReactNode,
-  onClick?: () => void,
-  className?: string,
-  children?: React.ReactNode
-}
+  iconLeft?: ReactElement;
+  iconRight?: ReactElement;
+  onClick?: () => void;
+  className?: string;
+  children?: React.ReactNode;
+};
 
-const IconButton = ({ iconLeft, iconRight, onClick, className, children }: IconButtonProps) => {
+const IconButton = ({
+  iconLeft,
+  iconRight,
+  onClick,
+  className,
+  children,
+}: IconButtonProps) => {
   return (
-    <div className={`iconButtonContainer ${className}`} onClick={onClick}>
-      <div className="icon left">
-        {iconLeft}
-      </div>
-      <div className="content">
+    <Button>
+      <div className={`iconButtonContainer ${className}`} onClick={onClick}>
+        {iconLeft && React.cloneElement(iconLeft, { className: 'icon left' })}
         {children}
+        {iconRight &&
+          React.cloneElement(iconRight, { className: 'icon right' })}
       </div>
-      <div className="icon right">
-        {iconRight}
-      </div>
-    </div>
-  )
-}
+    </Button>
+  );
+};
 
-export default IconButton
+export default IconButton;
