b8cba3500cd042b1631397f0651a275343338926
Jelle Baele <jellebaele@hotmail.com>
2023-10-04 19:37:27 +0200

    Reformat

--- client_v2/src/App.tsx
+++ client_v2/src/App.tsx
@@ -1,4 +1,4 @@
-import './assets/styles/app.scss'
+import './assets/styles/app.scss';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductsListPage from './pages/products/ProductsListPage';
@@ -14,24 +14,23 @@ function App() {
   const previousLocationModal = location.state?.previousLocation;
 
   return (
-    <div className='rootContainer'>
+    <div className="rootContainer">
       <Routes location={previousLocationModal || location}>
         <Route path="/" element={<Navbar />}>
           <Route index element={<HomePage />} />
           <Route path="products">
             <Route index element={<ProductsListPage />} />
-            <Route path='edit/:productId' element={<EditProductPage />} />
+            <Route path="edit/:productId" element={<EditProductPage />} />
           </Route>
-          <Route path='profile' element={<ProfilePage />} />
+          <Route path="profile" element={<ProfilePage />} />
           <Route path="*" element={<NotExistsPage />} />
         </Route>
-        <Route path='login' element={<LoginPage />} />
+        <Route path="login" element={<LoginPage />} />
       </Routes>
-
 
       {previousLocationModal && (
         <Routes>
-          <Route path='/products/:productId' element={<SingleProductPage />} />
+          <Route path="/products/:productId" element={<SingleProductPage />} />
         </Routes>
       )}
     </div>
