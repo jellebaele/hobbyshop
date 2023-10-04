2db62f842730e57df780507aab14f4428e9aee4e
Jelle Baele <jellebaele@hotmail.com>
2023-10-04 19:17:41 +0200

    Use buttonicon

--- client_v2/src/pages/HomePage.tsx
+++ client_v2/src/pages/HomePage.tsx
@@ -1,10 +1,10 @@
-import { Link } from "react-router-dom"
-import "../assets/styles/pages/homePage.scss"
-import Button from "../components/ui/Button"
+import { Link } from 'react-router-dom';
+import '../assets/styles/pages/homePage.scss';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import ProductList from "../features/products/ProductList";
-import Container from "../components/ui/Container";
-import Header from "../components/ui/Header";
+import ProductList from '../features/products/ProductList';
+import Container from '../components/ui/Container';
+import Header from '../components/ui/Header';
+import IconButton from '../components/ui/IconButton';
 
 const HomePage = () => {
   const renderHighlightedProducts = () => {
@@ -13,24 +13,19 @@ const HomePage = () => {
         <div className="top">
           <Header>Uitgelichte product </Header>
           <Link to={'/products'}>
-            <Button className='button'>
-              Alle producten <ArrowForwardIcon className='icon' />
-            </Button>
+            <IconButton iconRight={<ArrowForwardIcon />}>
+              Alle producten
+            </IconButton>
           </Link>
         </div>
         <div>
           <ProductList />
         </div>
       </Container>
+    );
+  };
 
-    )
-  }
+  return <div className="homeContainer">{renderHighlightedProducts()}</div>;
+};
 
-  return (
-    <div className='homeContainer'>
-      {renderHighlightedProducts()}
-    </div >
-  )
-}
-
-export default HomePage
+export default HomePage;
